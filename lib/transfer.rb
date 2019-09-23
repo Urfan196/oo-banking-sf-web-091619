0ef4dcbecef23eba09db272cec51ce20286cbd18
Urfan Guliyev <urfan196@yahoo.com>
2019-09-23 05:10:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ def valid?
 end
 
 def execute_transaction
-  if @status != "complete" && self.valid? && self.balance >= @amount
+  if @status != "complete" && self.valid? && @sender.balance >= @amount
   @sender.balance -= @amount
   @receiver.balance += @amount
   @status = "complete"
